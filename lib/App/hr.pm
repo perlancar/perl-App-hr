8808be037e0dfb1ea11cd3f4ab170fe64121816b
perlancar (@pc-office) <perlancar@gmail.com>
2015-07-02 21:16:59 +0700

    Convert script to use Perinci::CmdLine::Inline

--- lib/App/hr.pm
+++ lib/App/hr.pm
@@ -13,6 +13,10 @@ our @EXPORT_OK = qw(
                        hr
                );
 
+our %SPEC;
+
+# INSERT_BLOCK_FROM_MODULE: Function::Embeddable pick
+
 my $term_width;
 if (eval { require Term::Size; 1 }) {
     ($term_width, undef) = Term::Size::chars();
@@ -36,6 +40,114 @@ sub hr {
     say $hr;
 }
 
+$SPEC{hr_app} = {
+    v => 1.1,
+    summary => 'Print horizontal bar on the terminal',
+    description => <<'_',
+
+    % hr
+    ============================================================================
+
+    % hr -c red  ;# will output the same bar, but in red
+
+    % hr x----
+    x----x----x----x----x----x----x----x----x----x----x----x----x----x----x----x
+
+    % hr --random-pattern
+
+    % hr --random-color
+
+    % hr -r  ;# shortcut for --random-pattern --random-color
+
+    % hr -- -x-  ;# specify a pattern that starts with a dash
+    % hr -p -x-  ;# ditto
+
+    % hr --help
+
+You can also use the `hr` function in `App::hr` module.
+
+_
+    args_rels => {
+        'choose_one&' => [
+            [qw/color random_color/],
+            [qw/pattern random_pattern/],
+        ],
+    },
+    args => {
+        color => {
+            summary => 'Specify a color (see Term::ANSIColor)',
+            schema => 'str*',
+            cmdline_aliases => {c=>{}},
+        },
+        random_color => {
+            schema => ['bool', is=>1],
+        },
+        pattern => {
+            summary => 'Specify a pattern',
+            schema => 'str*',
+            pos => 0,
+            cmdline_aliases => {p=>{}},
+        },
+        random_pattern => {
+            schema => ['bool', is=>1],
+            cmdline_aliases => {
+                r => {
+                    summary => 'Alias for --random-pattern --random-color',
+                    is_flag => 1,
+                    code => sub {
+                        $_[0]{random_color} = 1;
+                        $_[0]{random_pattern} = 1;
+                    },
+                },
+            },
+        },
+    },
+};
+sub hr_app {
+    my %args = @_;
+
+    if ($args{random_color}) {
+        $args{color} = pick(
+            'red',
+            'bright_red',
+            'green',
+            'bright_green',
+            'blue',
+            'bright_blue',
+            'cyan',
+            'bright_cyan',
+            'magenta',
+            'bright_magenta',
+            'yellow',
+            'bright_yellow',
+            'white',
+            'bright_white',
+        );
+    }
+
+    if ($args{random_pattern}) {
+        $args{pattern} = pick(
+            '.',
+            '-',
+            '=',
+            'x',
+            'x-',
+            'x---',
+            'x-----',
+            '*',
+            '*-',
+            '*---',
+            '*-----',
+            '/\\',
+            'v',
+            'V',
+        );
+    }
+
+    [200, "OK", hr($args{pattern}, $args{color}),
+     {'cmdline.skip_format' => 1}];
+}
+
 1;
 # ABSTRACT: Print horizontal bar on the terminal
 
@@ -57,7 +169,7 @@ Sample output:
 You can also use the provided CLI L<hr>.
 
 
-=head1 FUNCTIONS
+=head1 FUNCTIONS_TMP
 
 =head2 hr([PATTERN]) => optional STR
 
